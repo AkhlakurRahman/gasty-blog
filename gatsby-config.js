2bfad1b1107785e4e763613ae425b38f37474c03
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-10-22 14:46:21 +0600

    navigation, home and about page added

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Dev Blog',
+    description:
+      'Where every developer find can read Web Development related article!',
+  },
+  plugins: ['gatsby-plugin-emotion'],
+};
